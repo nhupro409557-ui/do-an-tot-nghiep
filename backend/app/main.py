ed8da51cd4a6ff639bed5ab45bef3953ca5f2a7c
nhupro409557-ui <nhupro409557@gmail.com>
2026-06-12 22:13:10 +0700

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -159,6 +159,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
